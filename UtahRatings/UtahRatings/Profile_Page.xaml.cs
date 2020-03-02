318fa6b70b9203f3aed46dfad23acf6809ac325d
mayraserrato <serratomayra@outlook.com>
2020-03-02 15:23:01 -0700

    New project

---
+++ UtahRatings/UtahRatings/Profile_Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UtahRatings
+{
+    public partial class Profile_Page : ContentPage
+    {
+        public Profile_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
