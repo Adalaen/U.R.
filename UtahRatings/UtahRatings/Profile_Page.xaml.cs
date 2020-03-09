3c17b126b916d980a1f065e320d0c2315ed18c5b
mayraserrato <serratomayra@outlook.com>
2020-03-09 00:19:58 -0600

    Adding List Model

--- UtahRatings/UtahRatings/Profile_Page.xaml.cs
+++ UtahRatings/UtahRatings/Profile_Page.xaml.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using UtahRatings.Model;
 using Xamarin.Forms;
 
 namespace UtahRatings
 {
     public partial class Profile_Page : ContentPage
     {
+        public ObservableCollection<MyList_Model> My_List { get; set; }
+
         public Profile_Page()
         {
             InitializeComponent();
+
+            My_List = new ObservableCollection<MyList_Model>
+            {
+                new MyList_Model
+                {
+                    CategoryName = "Hikes",
+                    //Image = "HikeCategory.png",
+                },
+
+                new MyList_Model
+                {
+                    CategoryName = "Restaurants",
+                    //Image = "restaurantCategory.png",
+                },
+
+                new MyList_Model
+                {
+                    CategoryName = "Entertainment",
+                    //Image = "entertainmentCategory.png",
+                }
+            };
+            //Personal_List_View.ItemsSource = My_List;
         }
     }
 }
